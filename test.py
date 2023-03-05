7f24c55b2b697404fdf24ae276b61a2fb67ddf8d
khaoula-belguezzar <khawlabel2020@gmail.com>
2023-03-05 23:19:26 +0100

    ajouter une modification

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-print("Hello everyone !")
-print("So..! my name is khaoula and i'm 19 years old !")
-print("nice to meet you")
+# fonction 1
+def inc(x):
+    return x + 1
+
+#test fonction 1
+def test_answer():
+    assert inc(3) == 5
+    
+#fonction 2
+import datetime
+
+def get_age(yyyy:int, mm:int, dd:int) -> int:
+    dob = datetime.date(yyyy, mm, dd)
+    today = datetime.date.today()
+    age = round((today - dob).days / 365.25)
+    return age
+
+#test fonction 2
+def test_get_age():
+    # Given.
+    yyyy, mm, dd = map(int, "1996/07/11".split(""))   
+    # When.
+    age = get_age(yyyy, mm, dd)
+    # Then.
+    assert age == 26 
