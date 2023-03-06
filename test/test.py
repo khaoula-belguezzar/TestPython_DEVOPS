de2ca5a971c615cc26db5d418af0da4bc602fdbc
khaoula-belguezzar <khawlabel2020@gmail.com>
2023-03-06 21:07:27 +0100

    ..

--- test/test.py
+++ test/test.py
@@ -1,24 +1,11 @@
-# fonction 1
-def inc(x):
-    return x + 1
-
-#fonction 2
-import datetime
-
-def get_age(yyyy:int, mm:int, dd:int) -> int:
-    dob = datetime.date(yyyy, mm, dd)
-    today = datetime.date.today()
-    age = round((today - dob).days / 365.25)
-    return age
-
-#fonction 3
-def deinc(x):
-    return x - 1
-
+from app import inc
+from app import get_age
+from app import deinc
+from app import hehe
 #test fonction 1
 def test_answer1():
     assert inc(1) == 2
-    
+
 #test fonction 2
 def test_get_age():
     # Given.
